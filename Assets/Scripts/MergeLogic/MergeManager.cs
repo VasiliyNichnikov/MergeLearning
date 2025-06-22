851a0845dcb9ecf42e05ada84cb80f37d805a74f
v.nichnikov <57954863+VasiliyNichnikov@users.noreply.github.com>
2025-06-22 23:16:10 +0300

    Исправление ошибок

--- Assets/Scripts/MergeLogic/MergeManager.cs
+++ Assets/Scripts/MergeLogic/MergeManager.cs
@@ -1,26 +1,22 @@
-﻿using Data;
-using EnvLevel;
+﻿using EnvLevel;
 
 namespace MergeLogic
 {
     public class MergeManager
     {
         private readonly LevelGeneration _levelGeneration;
-        private readonly CubeColorsData _cubeColorsData;
         
-        public MergeManager(LevelGeneration levelGeneration, CubeColorsData cubeColorsData)
+        public MergeManager(LevelGeneration levelGeneration)
         {
             _levelGeneration = levelGeneration;
-            _cubeColorsData = cubeColorsData;
         }
 
         public void AddCube(ICubeController cube)
         {
             var initLevel = _levelGeneration.Generate();
-            var ballColor = _cubeColorsData.GetColor(initLevel);
             
             var changerColor = cube.ChangerColor;
-            changerColor.ApplyColor(ballColor);
+            changerColor.ApplyColor(initLevel);
         }
     }
 }
